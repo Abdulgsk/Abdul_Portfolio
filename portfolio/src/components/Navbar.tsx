a8db147f7b4f8b031e7d12b23a1e54498386eec8
Vikash M <abdul29112004@gmail.com>
2024-09-26 11:37:02 +0530

    leetcode

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import logo from "../assets/logo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaHackerrank } from "react-icons/fa6";
+import { SiLeetcode } from "react-icons/si";
 
 const Navbar = () => {
   return (
@@ -12,13 +13,11 @@ const Navbar = () => {
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href="https://www.linkedin.com/in/abdul-gouse-syeedy-000027277?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank"> <FaLinkedin/> </a>
         <a href="https://github.com/Abdulgsk" target="_blank"> <FaGithub/> </a>
-        <a href="https://leetcode.com/u/Abdul_Gouse/" target="_blank
-        "><img  width={24} height={24} src="https://leetcode.com/favicon.ico"
-        alt="LeetCode"/></a>
+        <a href="https://leetcode.com/u/Abdul_Gouse/"><SiLeetcode/></a>
         <a href="https://www.hackerrank.com/profile/abdul29112004" target="_blank"> <FaHackerrank/> </a>
       </div>
     </nav>
   )
-}           
+}          
  
 export default Navbar;
